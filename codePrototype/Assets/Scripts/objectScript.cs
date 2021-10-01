64f3dd4153fdeba58d9eae5f965f777d8eb0bedd
therealharrisonw <90920251+therealharrisonw@users.noreply.github.com>
2021-10-01 10:04:13 -0600

    Timer is working

--- codePrototype/Assets/Scripts/objectScript.cs
+++ codePrototype/Assets/Scripts/objectScript.cs
@@ -6,21 +6,17 @@ public class objectScript : MonoBehaviour
 {
     Renderer objRenderer;
 
-    public float timer = 60f;// acts as the timer 
+    
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer > 0f)// if timer is greater than 0, print text to console displaying time. 
-        {
-            timer -= Time.deltaTime;
-           print("Time:" + timer);
-        }
+        
     }
 
 }
---
+++ codePrototype/Assets/Scripts/timerScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timerScript : MonoBehaviour
+{
+    public float timer = 60f;// acts as the timer 
+    public bool runTime = false; // is used for deciding the bool for void 
+    // Start is called before the first frame update
+    void Start()
+    {
+        runTime = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (runTime == true)
+        {
+            if (timer > 0f)// if timer is greater than 0, print text to console displaying time. 
+            {
+                timer -= Time.deltaTime;
+                print("Time:" + timer);
+            }
+            else
+            {
+                runTime = false;
+                print("Time's Up shmuck");
+            }
+        }
+    }
+}
