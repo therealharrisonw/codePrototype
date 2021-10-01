da034763650c77d56717368900acaeb90491d87f
therealharrisonw <90920251+therealharrisonw@users.noreply.github.com>
2021-10-01 17:33:29 -0600

    started spawning and color change

--- codePrototype/Assets/Scripts/spawnManager.cs
+++ codePrototype/Assets/Scripts/spawnManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class spawnManager : MonoBehaviour
 {
+    GameObject[] spawnPoints;
+    [SerializeField]
+    GameObject prefabs;
     // Start is called before the first frame update
     void Start()
     {
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoints");
         
+        foreach (GameObject spawn in spawnPoints)
+        {
+            GameObject spawnObj = Instantiate(prefabs, spawn.transform, false);
+        }
     }
 
     // Update is called once per frame
