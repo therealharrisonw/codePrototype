7feb04e95714602b84aa101f758b7e1ff46af0b6
therealharrisonw <90920251+therealharrisonw@users.noreply.github.com>
2021-10-05 12:50:59 -0600

    Check the color of the random spawned objects

--- codePrototype/Assets/Scripts/spawnManager.cs
+++ codePrototype/Assets/Scripts/spawnManager.cs
@@ -16,6 +16,7 @@ public class spawnManager : MonoBehaviour
     [SerializeField]
     GameObject randprefabs;
     // create array of materials for "recipe"
+    Material[] materialArray;
 
     public GameObject spawncolor1;
     public GameObject spawncolor2;
@@ -44,6 +45,10 @@ public class spawnManager : MonoBehaviour
             GameObject spawnObj = Instantiate(randprefabs, spawn.transform, false);
             // make sure array name is the same as you will setup at the top
             //materialArray[i] = spawnObj.GetComponent<Renderer>().material;
+            for (int i = 0; i <= 4; i = i++  )
+            {
+                materialArray[i] = spawnObj.GetComponent<Renderer>().material;
+            }
         }
 
         
